01ad2316c7a77373c3fe02bd2cad000017f6b376
Tkz-tikrazix <tikrazix.pro@gmail.com>
2022-06-23 06:38:01 -0400

    Initial commit

---
+++ fxserver.sql
@@ -0,0 +1,377 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 23 juin 2022 à 12:22
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `fxserver`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_account`
+--
+
+CREATE TABLE `addon_account` (
+  `name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `label` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_account_data`
+--
+
+CREATE TABLE `addon_account_data` (
+  `id` int(11) NOT NULL,
+  `account_name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `money` int(11) NOT NULL,
+  `owner` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_inventory`
+--
+
+CREATE TABLE `addon_inventory` (
+  `name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `label` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_inventory_items`
+--
+
+CREATE TABLE `addon_inventory_items` (
+  `id` int(11) NOT NULL,
+  `inventory_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `count` int(11) NOT NULL,
+  `owner` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billing`
+--
+
+CREATE TABLE `billing` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sender` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `target_type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `target` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `label` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datastore`
+--
+
+CREATE TABLE `datastore` (
+  `name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `label` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datastore_data`
+--
+
+CREATE TABLE `datastore_data` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `owner` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `data` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `items`
+--
+
+CREATE TABLE `items` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `weight` int(11) NOT NULL DEFAULT 1,
+  `rare` tinyint(4) NOT NULL DEFAULT 0,
+  `can_remove` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `jobs`
+--
+
+INSERT INTO `jobs` (`name`, `label`) VALUES
+('unemployed', 'Unemployed');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `job_grades`
+--
+
+CREATE TABLE `job_grades` (
+  `id` int(11) NOT NULL,
+  `job_name` varchar(50) DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext NOT NULL,
+  `skin_female` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `job_grades`
+--
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(1, 'unemployed', 0, 'unemployed', 'Unemployed', 200, '{}', '{}');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `licenses`
+--
+
+CREATE TABLE `licenses` (
+  `type` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `label` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `society_moneywash`
+--
+
+CREATE TABLE `society_moneywash` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `society` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `identifier` varchar(60) NOT NULL,
+  `accounts` longtext DEFAULT NULL,
+  `group` varchar(50) DEFAULT 'user',
+  `inventory` longtext DEFAULT NULL,
+  `job` varchar(20) DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `loadout` longtext DEFAULT NULL,
+  `position` varchar(255) DEFAULT '{"x":-269.4,"y":-955.3,"z":31.2,"heading":205.8}',
+  `skin` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`identifier`, `accounts`, `group`, `inventory`, `job`, `job_grade`, `loadout`, `position`, `skin`) VALUES
+('03d18118b2fa4204e43fbf27fa87f32ea08d2f91', '{\"bank\":5200,\"black_money\":0,\"money\":500}', 'admin', '[]', 'unemployed', 0, '[]', '{\"y\":-1638.3,\"z\":28.7,\"heading\":359.4,\"x\":156.6}', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_licenses`
+--
+
+CREATE TABLE `user_licenses` (
+  `id` int(11) NOT NULL,
+  `type` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `owner` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `addon_account`
+--
+ALTER TABLE `addon_account`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  ADD KEY `index_addon_account_data_account_name` (`account_name`);
+
+--
+-- Index pour la table `addon_inventory`
+--
+ALTER TABLE `addon_inventory`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  ADD KEY `index_addon_inventory_inventory_name` (`inventory_name`);
+
+--
+-- Index pour la table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `datastore`
+--
+ALTER TABLE `datastore`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  ADD KEY `index_datastore_data_name` (`name`);
+
+--
+-- Index pour la table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD PRIMARY KEY (`type`);
+
+--
+-- Index pour la table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- Index pour la table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
